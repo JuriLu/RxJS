938f0b88872a4d7616a9f314cf4e97df417f02a5
JuriLu <lubonjajurgen1@gmail.com>
2023-05-10 12:33:44 +0200

    Adding data async + understanding better the observer + the lifecycle including complete

--- index.ts
+++ index.ts
@@ -2,14 +2,32 @@ import { Observable } from 'rxjs';
 
 const observable$ = new Observable((subscriber) => {
   console.log('Observable executed');
+
   subscriber.next('Alice Added Synchronously');
   subscriber.next('Ben Added Synchronously');
-  setTimeout(
-    () => subscriber.next('**Charlie Added after 2s Asynchronously'),
-    2000
-  );
+  setTimeout(() => {
+    subscriber.next('**Charlie Added after 2s Asynchronously');
+    subscriber.complete();
+  }, 2000);
+
+
 });
 
 console.log(`--'Before Execute'--`);
-observable$.subscribe((value) => console.log(value));
+observable$.subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log(`----'Completed'----`),
+});
 console.log(`--'After Execute'--`);
+
+
+// So simplified what subscriber method had as an argument is an observer object  "subscribe(observer?: Partial<Observer<T>>): Subscription;" which has an Observer interface 
+
+export interface Observer<T> {
+    next: (value: T) => void;
+    error: (err: any) => void;
+    complete: () => void;
+} 
+
+
+//After the Subscription has ended the 'Teardown Logic'
