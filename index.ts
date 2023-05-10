93b18c87ae2c02b290f98edc21ebdfb16c7a5b10
JuriLu <lubonjajurgen1@gmail.com>
2023-05-10 12:05:28 +0200

    Observable lifecycle

--- index.ts
+++ index.ts
@@ -1,25 +1,9 @@
 import { Observable } from 'rxjs';
 
-const observable$ = new Observable<string>((subscriber) => {
-  console.log(`'Observable Executed'`);
-  subscriber.next('alice');
-  setTimeout(() => subscriber.next('ben'), 2000);
-  setTimeout(() => subscriber.next('charlie'), 4000);
+const observable$ = new Observable((subscriber) => {
+  console.log('Observable executed');
 });
 
-const observer = {
-  next: (value) => console.log(value),
-};
-
-//Method 1
-// observable$.subscribe(observer);
-
-//Method 2
-const subscription = observable$.subscribe((val) => {
-  console.log(val);
-});
-
-setTimeout(() => {
-  console.log(`'Unsubscribe'`);
-  subscription.unsubscribe();
-}, 3000);
+console.log(`'Before Execute'`);
+observable$.subscribe();
+console.log(`'After Execute'`);
