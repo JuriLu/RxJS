cda02ee6a3addcc009cd6f6ec8e6374ec9f6d558
JuriLu <lubonjajurgen1@gmail.com>
2023-05-10 12:12:59 +0200

    Adding values asynchronously, difference between sync and Async

--- index.ts
+++ index.ts
@@ -2,9 +2,14 @@ import { Observable } from 'rxjs';
 
 const observable$ = new Observable((subscriber) => {
   console.log('Observable executed');
-  subscriber.next('Alice Added');
+  subscriber.next('Alice Added Synchronously');
+  subscriber.next('Ben Added Synchronously');
+  setTimeout(
+    () => subscriber.next('**Charlie Added after 2s Asynchronously'),
+    2000
+  );
 });
 
-console.log(`'Before Execute'`);
+console.log(`--'Before Execute'--`);
 observable$.subscribe((value) => console.log(value));
-console.log(`'After Execute'`);
+console.log(`--'After Execute'--`);
