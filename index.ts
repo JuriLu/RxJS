ac3c3b91042be5fdeef46e4a6fdf9c8c4aa50776
JuriLu <lubonjajurgen1@gmail.com>
2023-05-11 11:46:09 +0200

    Teardown logic ran, observable is created successfullyn in full

--- index.ts
+++ index.ts
@@ -3,10 +3,14 @@ import { Observable, Subscriber } from 'rxjs';
 const interval$ = new Observable<number>((subscriber) => {
   let counter = 1;
 
-  setInterval(() => {
+  const intervalId = setInterval(() => {
     console.log('Emmited, ', counter);
     subscriber.next(counter++);
   }, 1000);
+
+  return () => {
+    clearInterval(intervalId);
+  };
 });
 
 const subscription = interval$.subscribe((value) => console.log(value));
