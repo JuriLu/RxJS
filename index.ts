9e951f81a058dcb29c2fdd8db77f6facc0f8463b
JuriLu <lubonjajurgen1@gmail.com>
2023-05-13 15:46:31 +0200

    LATEST UPDATES OF TAP WITH RXJS 7.3.0

--- index.ts
+++ index.ts
@@ -4,7 +4,14 @@ import { filter, map, tap } from 'rxjs/operators';
 of(1, 7, 3, 2, 6)
   .pipe(
     filter((value) => value > 5),
-    tap((value) => console.log('Spy :', value)),
+    tap({
+      next: (value) => console.log('Spy :', value),
+      complete: () => console.log('Completed'),
+      error: (err) => console.log(err),
+      // subscribe: () => { console.log('New Subscription'); },  // PART OF RXJS 7.3.0
+      // unsubscribe: () => { console.log('Unsubscribed'); },    // PART OF RXJS 7.3.0
+      // finalize: () => { console.log('Subscription ended'); }  // PART OF RXJS 7.3.0
+    }),
     map((value) => value * 2)
   )
   .subscribe((value) => console.log('Output:', value));
