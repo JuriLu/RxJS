593d37efb9110db2f083fb48ae63ed644ca3b36f
JuriLu <lubonjajurgen1@gmail.com>
2023-05-13 19:59:55 +0200

    Manage errors with flattening operators

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { fromEvent } from 'rxjs';
+import { EMPTY, fromEvent, of } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { concatMap, map } from 'rxjs/operators';
+import { catchError, concatMap, map, tap } from 'rxjs/operators';
 
 const endpointInput: HTMLInputElement =
   document.querySelector('input#endpoint');
@@ -10,9 +10,20 @@ fromEvent(fetchButton, 'click')
   .pipe(
     map(() => endpointInput.value),
     concatMap((value) =>
-      ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+      ajax(`https://random-data-api.com/api/${value}/random_${value}`).pipe(
+        catchError(() =>
+          of('Could not fetch data').pipe(
+            tap((val) => {
+              alert(val);
+              //can show primeNG toast
+            })
+          )
+        )
+      )
     )
   )
-  .subscribe((val) => {
-    console.log(val);
+  .subscribe({
+    next: (val) => console.log(val),
+    error: (err) => console.log('Error:', err),
+    complete: () => console.log('Completed'),
   });
