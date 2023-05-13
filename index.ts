a072b090421f95e67ae860a79876d6b2e05c506d
JuriLu <lubonjajurgen1@gmail.com>
2023-05-13 11:33:22 +0200

    From Creation function

--- index.ts
+++ index.ts
@@ -1,34 +1,19 @@
-import { Observable, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 
-of('Alice', 'Ben', 'Charlie').subscribe({
+from(['Alice', 'Ben', 'Charlie']).subscribe({
   next: (values) => console.log(values),
   complete: () => console.log('== Completed =='),
 });
 
-const names$ = new Observable<string>((subscriber) => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+const somePromise = new Promise((resolve, reject) => {
+  // resolve('-- Promise Resolved --');
+  reject('== Promise Rejected ==');
 });
 
-console.log(`'CUSTOM OBSERVABLE IMPLEMENTING 'OF' ':  `);
-names$.subscribe({
-  next: (values) => console.log(values),
-  complete: () => console.log('== Completed =='),
-});
+const observableFromPromise$ = from(somePromise);
 
-function ourOwnOf(...args: string[]): Observable<string> {
-  return new Observable<string>((subscriber) => {
-    for (let i = 0; i < args.length; i++) {
-      subscriber.next(args[i]);
-    }
-    subscriber.complete();
-  });
-}
-
-console.log(`'CUSTOM FUNCTION IMPLEMENTING 'OF creation function' ':  `);
-
-ourOwnOf('Alice 2', 'Ben 2', 'Charlie 2').subscribe((values) => {
-  console.log(values);
+observableFromPromise$.subscribe({
+  next: (value) => console.log(value),
+  error: (err) => console.log('Error: ', err),
+  complete: () => console.log('-- Completed --'),
 });
