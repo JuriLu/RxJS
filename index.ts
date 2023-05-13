b690f44dca7571b61bef866dd4fa271ab38e98d7
JuriLu <lubonjajurgen1@gmail.com>
2023-05-13 19:38:33 +0200

    concatMap with http

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
-import { Observable, of } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { concatMap, map } from 'rxjs/operators';
 
-const source$ = new Observable((subscriber) => {
-  setTimeout(() => {
-    subscriber.next('A');
-  }, 2000);
-  setTimeout(() => {
-    subscriber.next('B');
-  }, 5000);
-});
+const endpointInput: HTMLInputElement =
+  document.querySelector('input#endpoint');
+const fetchButton = document.querySelector('button#fetch');
 
-console.log('App has started');
-source$
-  .pipe(concatMap((value) => of(1, 2)))
-  .subscribe((value) => console.log(value));
-
-// FOR THE VALUE A AND B EMITTED FROM SOURCE$ OBSERVABLE, VALUES 1, 2 ARE EMITTED FROM OF(1,2) OBSERVABLE
+fromEvent(fetchButton, 'click')
+  .pipe(
+    map(() => endpointInput.value),
+    concatMap((value) =>
+      ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+    )
+  )
+  .subscribe((val) => {
+    console.log(val);
+  });
