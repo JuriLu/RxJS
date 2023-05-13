f689432818ca592d3d59865f90a610796cb9217d
JuriLu <lubonjajurgen1@gmail.com>
2023-05-13 12:17:26 +0200

    Implementing teardown logic

--- index.ts
+++ index.ts
@@ -2,17 +2,32 @@ import { from, fromEvent, Observable } from 'rxjs';
 
 const triggerButton = document.querySelector('button#trigger');
 
-fromEvent<MouseEvent>(triggerButton, 'click').subscribe((event) =>
-  console.log(event, event.type, event.x, event.y)
+const subscription = fromEvent<MouseEvent>(triggerButton, 'click').subscribe(
+  (event) => console.log(event, event.type, event.x, event.y)
 );
 
 const triggerClick$ = new Observable((subscriber) => {
-  triggerButton.addEventListener('click', (event) => {
+  const clickHandlerFn = (event) => {
+    console.log('Event Callback Executed');
     subscriber.next(event);
-  });
+  };
+
+  triggerButton.addEventListener('click', clickHandlerFn);
+
+  return () => {
+    triggerButton.removeEventListener('click', clickHandlerFn);
+  };
 });
 
-triggerClick$.subscribe((ev: MouseEvent) => {
+const subscriptionCustom = triggerClick$.subscribe((ev: MouseEvent) => {
   console.log('Custom Subscription: ');
   console.log(ev);
 });
+
+setTimeout(() => {
+  console.log('Unsubscribing Sub');
+  subscription.unsubscribe();
+
+  console.log('Unsubscribing CustomSub');
+  subscriptionCustom.unsubscribe();
+}, 3000);
