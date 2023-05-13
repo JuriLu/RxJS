f5ae0f54ba724f8d82c88ad3b2e4f4b2177f7f41
JuriLu <lubonjajurgen1@gmail.com>
2023-05-13 18:58:06 +0200

    debounceTime

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
-import { of } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
-of(1, 7, 3, 2, 6)
+const sliderInput = document.querySelector('input#slider');
+
+fromEvent(sliderInput, 'input')
   .pipe(
-    filter((value) => value > 5),
-    tap({
-      next: (value) => console.log('Spy :', value),
-      complete: () => console.log('Completed'),
-      error: (err) => console.log(err),
-      // subscribe: () => { console.log('New Subscription'); },  // PART OF RXJS 7.3.0
-      // unsubscribe: () => { console.log('Unsubscribed'); },    // PART OF RXJS 7.3.0
-      // finalize: () => { console.log('Subscription ended'); }  // PART OF RXJS 7.3.0
-    }),
-    map((value) => value * 2)
+    debounceTime(1000),
+    map((event) => event.target['value'])
   )
-  .subscribe((value) => console.log('Output:', value));
+  .subscribe((val) => {
+    console.log(val);
+  });
