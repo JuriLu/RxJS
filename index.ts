153493a7ec0b589cd27a37990d309097a5821f7b
JuriLu <lubonjajurgen1@gmail.com>
2023-05-13 10:01:19 +0200

    'of' Creation Function

--- index.ts
+++ index.ts
@@ -1,20 +1,34 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
-const helloButton = document.querySelector('button#hello');
-
-const helloClick$ = new Observable<MouseEvent>((subscriber) => {
-  helloButton.addEventListener('click', (event: MouseEvent) => {
-    subscriber.next(event);
-  });
+of('Alice', 'Ben', 'Charlie').subscribe({
+  next: (values) => console.log(values),
+  complete: () => console.log('== Completed =='),
 });
 
-helloClick$.subscribe((ev) => console.log('Sub 1', ev.type, ev.x, ev.y));
-helloClick$.subscribe((ev) => console.log('Sub 2', ev.type, ev.x, ev.y));
+const names$ = new Observable<string>((subscriber) => {
+  subscriber.next('Alice');
+  subscriber.next('Ben');
+  subscriber.next('Charlie');
+  subscriber.complete();
+});
 
-setTimeout(() => {
-  console.log('Sub 3 Started');
-  helloClick$.subscribe((ev) => console.log('Sub 3', ev.type, ev.x, ev.y));
+console.log(`'CUSTOM OBSERVABLE IMPLEMENTING 'OF' ':  `);
+names$.subscribe({
+  next: (values) => console.log(values),
+  complete: () => console.log('== Completed =='),
+});
 
-  console.log('Sub 4 Started');
-  helloClick$.subscribe((ev) => console.log('Sub 4', ev.type, ev.x, ev.y));
-}, 4000);
+function ourOwnOf(...args: string[]): Observable<string> {
+  return new Observable<string>((subscriber) => {
+    for (let i = 0; i < args.length; i++) {
+      subscriber.next(args[i]);
+    }
+    subscriber.complete();
+  });
+}
+
+console.log(`'CUSTOM FUNCTION IMPLEMENTING 'OF creation function' ':  `);
+
+ourOwnOf('Alice 2', 'Ben 2', 'Charlie 2').subscribe((values) => {
+  console.log(values);
+});
