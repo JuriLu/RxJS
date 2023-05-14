1e7283a729a0d272815e28fc9c7f0ddeecf55c37
JuriLu <lubonjajurgen1@gmail.com>
2023-05-14 13:34:58 +0200

    Subject

--- index.ts
+++ index.ts
@@ -1,29 +1,17 @@
-import { EMPTY, fromEvent, of } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
-import { catchError, concatMap, map, tap } from 'rxjs/operators';
+import { fromEvent, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-const endpointInput: HTMLInputElement =
-  document.querySelector('input#endpoint');
-const fetchButton = document.querySelector('button#fetch');
+const emitButton = document.querySelector('button#emit');
+const inputElement: HTMLInputElement = document.querySelector('#value-input');
+const subscribeButton = document.querySelector('button#subscribe');
 
-fromEvent(fetchButton, 'click')
-  .pipe(
-    map(() => endpointInput.value),
-    concatMap((value) =>
-      ajax(`https://random-data-api.com/api/${value}/random_${value}`).pipe(
-        catchError(() =>
-          of('Could not fetch data').pipe(
-            tap((val) => {
-              alert(val);
-              //can show primeNG toast
-            })
-          )
-        )
-      )
-    )
-  )
-  .subscribe({
-    next: (val) => console.log(val),
-    error: (err) => console.log('Error:', err),
-    complete: () => console.log('Completed'),
-  });
+const value$ = new Subject<string>();
+
+fromEvent(emitButton, 'click')
+  .pipe(map(() => inputElement.value))
+  .subscribe(value$);
+
+fromEvent(subscribeButton, 'click').subscribe(() => {
+  console.log('New Subscription');
+  value$.subscribe((value) => console.log(value));
+});
